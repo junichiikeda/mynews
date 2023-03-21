13a28800c428faede6162ae4d96d13a20e64334a
j-ikeda <spn492f9@dune.ocn.ne.jp>
2023-03-21 01:59:51 +0000

    07 ユーザー認証を実装する

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// ログインしていない状態で管理画面にアクセスしようとしたときに、ログイン画面にリダイレクト。middleware以下追記
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });   
 
 // 課題3
-Route::controller(AAAController::class)->prefix('admin')->group(function() {
+/*Route::controller(AAAController::class)->prefix('admin')->group(function() {
     Route::get('bbb', 'Action');
-}); 
+}); */
 
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add');
-    Route::get('profile/edit', 'edit');
+    Route::get('profile/create', 'add')->middleware('auth');
+    Route::get('profile/edit', 'edit')->middleware('auth');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
