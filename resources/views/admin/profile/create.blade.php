1f0dc3dda5d12e18d40b8033360719d4532487e8
j-ikeda <spn492f9@dune.ocn.ne.jp>
2023-03-22 13:17:19 +0000

    08 ニュース投稿画面を作成しよう.

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,17 +1,27 @@
-{{-- layouts/profile.blade.phpを読み込む --}}
-@extends('layouts.profile')
-
-
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'プロフィール作成画面')
-
-{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
-@section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 mx-auto">
-                <h2>My プロフィール</h2>
-            </div>
-        </div>
-    </div>
-@endsection
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form</title>
+</head>
+<body>
+    <article>
+        <h1>プロフィール入力フォーム</h1>
+        <p>下記入力フォームよりアンケートのご入力をお願いします。</p>
+            <form action="{{ route('Admin.ProfileController. create') }}" method="post">
+            <P><label>お名前(name)</br>
+            <input type=""text name="name"></label></P>
+            <p>性別(gender)
+                <label><input type="radio" name="gender" value="male">男性</label>
+                <label><input type="radio" name="gender" value="female">女性</label>
+            </p>
+            <p><label>趣味(hobby)</br>
+            <textarea name="comment" cols="30" rows="5"></textarea></label></p>
+            <p><label>自己紹介(introduction)</br>
+            <textarea name="comment" cols="30" rows="20" placeholder="こちらに入力してください"></textarea></label></p>
+        
+        </form>
+    </article>
+</body>
+</html>
