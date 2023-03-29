347f512beaf20ff2683af577e409d8d6197d6f93
j-ikeda <spn492f9@dune.ocn.ne.jp>
2023-03-29 03:54:06 +0000

    09 投稿データを保存しよう 課題6

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -6,6 +6,14 @@
         <h1></h1>
         <p>下記フォームよりプロフィールのご入力をお願いします。</p>
             <form action="{{ route('admin.profile.create') }}" method="post">
+                
+            @if (count($errors) > 0)
+                <ul>
+                    @foreach($errors->all() as $e)
+                        <li>{{ $e }}</li>
+                    @endforeach
+                </ul>
+            @endif
             
             <P><label>お名前(name)</br>
             <input type=""text name="name"></label></P>
@@ -17,7 +25,8 @@
             <textarea name="comment" cols="30" rows="5"></textarea></label></p>
             <p><label>自己紹介(introduction)</br>
             <textarea name="comment" cols="30" rows="20" placeholder="こちらに入力してください"></textarea></label></p>
-        
+            @csrf
+            <input type="submit" class="btn btn-primary" value="更新">
         </form>
     </article>
 @endsection
