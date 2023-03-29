57287351365037552a068e08c367ba9f8f81ad41
j-ikeda <spn492f9@dune.ocn.ne.jp>
2023-03-29 03:55:26 +0000

    09 投稿データを保存しよう 課題7

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,18 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Profile;
 
 class ProfileController extends Controller
 {
     // 追記
-      public function add()
+    public function add()
     {
         return view('admin.profile.create');
-    }
-
-    public function create()
-    {
-        return redirect('admin/profile/create');
     }
 
     public function edit()
@@ -23,15 +19,33 @@ class ProfileController extends Controller
         return view('admin.profile.edit');
     }
 
-    public function update()
+    /* public function update()
     {
         return redirect('admin/profile/edit');
-    }
+    } */
     
     // 08 ニュース投稿画面を作成しよう 課題6
     public function update()
     {
         return view('admin.profile.edit');
-    }
+    } 
+    
+    public function create(Request $request)
+    {
+         // Validationを行う
+        $this->validate($request, Profile::$rules);
+        
+        $profile = new Profile;
+        $form = $request->all();
 
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+       
+        // admin/profile/createにリダイレクトする
+        return redirect('admin/profile/create');
+    }
 }
