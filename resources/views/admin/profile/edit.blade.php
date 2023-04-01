c7ff228e78fef47868fc24c2e9f075721d23f9ca
j-ikeda <spn492f9@dune.ocn.ne.jp>
2023-04-01 12:24:44 +0000

    11 投稿したニュースを更新/削除しよう 課題1

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,14 +1,23 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
+@section('title', 'プロフィールの新規作成')
 
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>"edit.blade.php"</h1>
-        <h2>"エディットです"</h2>
-    </body>
-</html>
+@section('content')
+
+    <div class="container">
+        <h2下記フォームよりプロフィールのご入力をお願いします。></h2>
+        <form action="{{ route('admin.profile.update') }}" method="post">
+            <P><label>お名前(name)</br>
+            <input type=""text name="name"></label></P>
+            <p>性別(gender)
+                <label><input type="radio" name="gender" value="male">男性</label>
+                <label><input type="radio" name="gender" value="female">女性</label>
+            </p>
+            <p><label>趣味(hobby)</br>
+            <textarea name="hobby" cols="30" rows="5"></textarea></label></p>
+            <p><label>自己紹介(introduction)</br>
+            <textarea name="introduction" cols="30" rows="15" placeholder="こちらに入力してください"></textarea></label></p>
+            @csrf
+            <input type="submit" class="btn btn-primary" value="更新">
+        </form>
+    </div>
+@endsection
